08e2815d55b635870bc9f2fbf246ccda039cec39
olkazavr <123472307+olkazavr@users.noreply.github.com>
2024-05-15 23:40:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,40 +16,39 @@ function play() {
         Swal.fire({
             icon: "error",
             title: "Ой!",
-            text: "Введи число от 1 до 20!",
+            text: "Введи число від 1 до 20!",
         });
     }
     else if (isNaN(userNumber)) {
         Swal.fire({
             icon: "error",
             title: "Ой!",
-            text: "Нужно ввести число!",
+            text: "Потрібно ввести число!",
         });
     }
     else {
         if(userNumber < answer) {
             Swal.fire({
                 icon: "error",
-                title: "Введи число выше!",
-                text: "Пока побеждает компьютер",
+                title: "Введи число вище!",
+                text: "Поки що перемагає комп'ютер",
             });
         }
         else if(userNumber > answer) {
             Swal.fire({
                 icon: "error",
-                title: "Введи число ниже!",
-                text: "Пока побеждает компьютер",
+                title: "Введи число нище!",
+                text: "Поки що перемагає комп'ютер",
             });
         }
         else {
             Swal.fire({
-                title: "Ура, ты победил!",
+                title: "Ура, ти виграв!",
                 imageUrl: "https://images.unsplash.com/photo-1531686264889-56fdcabd163f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 imageWidth: 400,
                 imageHeight: 300,
                 imageAlt: "Custom image"
             });
         }
-
     }
 }
